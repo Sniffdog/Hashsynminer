22a26b1c98190eaa32a9fc8fcb2d2e91b71b215b
Sniffdog <30452757+Sniffdog@users.noreply.github.com>
2018-01-08 20:34:49 -0700

    Update CcminerTpruvot1.ps1

--- Miners/CcminerTpruvot1.ps1
+++ Miners/CcminerTpruvot1.ps1
@@ -78,7 +78,7 @@ $Algorithms | Get-Member -MemberType NoteProperty | Select -ExpandProperty Name 
     [PSCustomObject]@{
         Type = 'NVIDIA1'
         Path = $Path
-        Arguments = -Join ('-a ', $Algorithms.$_, ' -o stratum+tcp://$($Pools.', $_, '.Host):$($Pools.', $_, '.Port) -b 0.0.0.0:4069 -u $($Pools.', $_, '.User1)  -p $($Pools.', $_, '.Pass1)', $Optimizations.$_)
+        Arguments = -Join ('-a ', $Algorithms.$_, ' -o stratum+tcp://$($Pools.', $_, '.Host):$($Pools.', $_, '.Port) -b 0.0.0.0:4069 -u $($Pools.', $_, '.User1) -p $($Pools.', $_, '.Pass1)', $Optimizations.$_)
         HashRates = [PSCustomObject]@{$_ = -Join ('$($Stats.', $Name, '_', $_, '_HashRate.Week)')}
         API = 'Ccminer'
         Port = 4069
