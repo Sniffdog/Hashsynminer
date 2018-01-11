b66185ae8c69ae99551b6aba7a903ad48b3a1e94
Sniffdog <30452757+Sniffdog@users.noreply.github.com>
2018-01-11 13:32:22 -0600

    Add files via upload

---
+++ Profit_Analyser/Hashsynminer_start_Zpool_profit_analyser.bat
@@ -0,0 +1,1 @@
+powershell -version 5.0 -noexit -executionpolicy bypass -windowstyle maximized -command "&.\Hashsynminer.ps1 -Username Tyredas -Workername Beeboop -RigName Hashsynminer001 -Currency USD -Passwordcurrency BTC -interval 90 -Delay 1 -SplitSniffCC0 '0' -SplitSniffEWBF1 '0' -Wallet 1AMQg6m9GPDN9HGuC3wJGpSuiZr1XQXjxi -Location US -PoolName zpool -Type nvidia -Algorithm x17 -Donate 5
