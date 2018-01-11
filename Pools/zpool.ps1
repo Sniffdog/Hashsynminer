6d15eb07c4d298c3769aac72625373577919d08f
Sniffdog <30452757+Sniffdog@users.noreply.github.com>
2018-01-11 13:21:29 -0600

    Add files via upload

---
+++ Pools/zpool.ps1
@@ -0,0 +1,70 @@
+﻿. .\Include.ps1
+
+$Name = Get-Item $MyInvocation.MyCommand.Path | Select-Object -ExpandProperty BaseName 
+ 
+ 
+$Zpool_Request = [PSCustomObject]@{} 
+ 
+ 
+ try { 
+     $Zpool_Request = Invoke-RestMethod "http://www.zpool.ca/api/status" -UseBasicParsing -TimeoutSec 10 -ErrorAction Stop 
+ } 
+ catch { 
+     Write-Log -Level Warn "Pool API ($Name) has failed. " 
+     return 
+ } 
+
+$Location = "US"
+
+$Zpool_Request | Get-Member -MemberType NoteProperty -ErrorAction Ignore | Select-Object -ExpandProperty Name | Where-Object {$Zpool_Request.$_.hashrate -gt 0} | foreach {
+    $Zpool_Host = "$_.mine.zpool.ca"
+    $Zpool_Port = $Zpool_Request.$_.port
+    $Zpool_Algorithm = Get-Algorithm $Zpool_Request.$_.name
+    $Zpool_Coin = $Zpool_Request.$_.coins
+
+    $Divisor = 1000000
+	
+    switch($Zpool_Algorithm)
+    {
+        "equihash"{$Divisor /= 1000}
+        "blake2s"{$Divisor *= 1000}
+	    "blakecoin"{$Divisor *= 1000}
+        "decred"{$Divisor *= 1000}
+	    "x11"{$Divisor *= 100}
+	    "keccak"{$Divisor *= 1000}
+    }
+
+    if((Get-Stat -Name "$($Name)_$($Zpool_Algorithm)_Profit") -eq $null){$Stat = Set-Stat -Name "$($Name)_$($Zpool_Algorithm)_Profit" -Value ([Double]$Zpool_Request.$_.estimate_last24h/$Divisor)}
+    else{$Stat = Set-Stat -Name "$($Name)_$($Zpool_Algorithm)_Profit" -Value ([Double]$Zpool_Request.$_.estimate_current/$Divisor)}
+	
+    if($Wallet)
+    {
+        [PSCustomObject]@{
+            Algorithm = $Zpool_Algorithm
+            Info = "$Zpool_Coin-coin(s)"
+            Price = $Stat.Live
+            StablePrice = $Stat.Week
+            MarginOfError = $Stat.Fluctuation
+            Protocol = "stratum+tcp"
+            Host = $Zpool_Host
+            Port = $Zpool_Port
+            User = $Wallet
+            User1 = $Wallet1
+	        User2 = $Wallet2
+	        User3 = $Wallet3
+	        User4 = $Wallet4
+	        User5 = $Wallet5
+	        User6 = $Wallet6
+	        User7 = $Wallet7
+            Pass = "ID=$RigName,c=$Passwordcurrency"
+            Pass1 = "ID=$RigName,c=$Passwordcurrency1"
+	        Pass2 = "ID=$RigName,c=$Passwordcurrency2"
+	        Pass3 = "ID=$RigName,c=$Passwordcurrency3"
+	        Pass4 = "ID=$RigName,c=$Passwordcurrency4"
+	        Pass5 = "ID=$RigName,c=$Passwordcurrency5"
+	        Pass6 = "ID=$RigName,c=$Passwordcurrency6"
+            Location = $Location
+            SSL = $false
+        }
+    }
+}
