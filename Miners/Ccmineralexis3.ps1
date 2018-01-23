6c0ff0b51ceeb4764271a85c1316d8488182578c
Sniffdog <30452757+Sniffdog@users.noreply.github.com>
2018-01-22 21:07:14 -0700

    Update Ccmineralexis3.ps1

--- Miners/Ccmineralexis3.ps1
+++ Miners/Ccmineralexis3.ps1
@@ -64,7 +64,7 @@ $Optimizations = [PSCustomObject]@{
     Timetravel = ' -i 25 -d $SplitSniffCC3'
     Blakecoin = ' -d $SplitSniffCC3'
     Lbry = ' -i 28 -d $SplitSniffCC3'
-    C11 = ' -d $SplitSniffCC3'
+    C11 = ' -i 20 -d $SplitSniffCC3'
     Nist5 = ' -i 25 -d $SplitSniffCC3'
     Hsr = ' -d $SplitSniffCC3'
 }
