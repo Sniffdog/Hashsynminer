beae01a42fe1d5d2934f7da04fa90c3891813b06
Sniffdog <30452757+Sniffdog@users.noreply.github.com>
2018-04-21 21:03:21 -0600

    Update CcminerRavenEnemy2.ps1

--- Miners/CcminerRavenEnemy2.ps1
+++ Miners/CcminerRavenEnemy2.ps1
@@ -9,6 +9,7 @@ $Algorithms = [PSCustomObject]@{
     
     X17 = 'x17'
     X16r = 'x16r'
+    X16s = 'x16s'
     Phi = 'phi'
     Bitcore = 'bitcore'
 }
@@ -17,6 +18,7 @@ $Optimizations = [PSCustomObject]@{
    
     X17 = ' -d $SplitSniffCC2'
     X16r = ' -d $SplitSniffCC2'
+    X16s = ' -d $SplitSniffCC2'
     Phi = ' -d $SplitSniffCC2'
     Bitcore = ' -d $SplitSniffCC2'
     
