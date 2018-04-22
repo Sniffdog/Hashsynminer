2bafabb818f0af44a8f902165409470245401f92
Sniffdog <30452757+Sniffdog@users.noreply.github.com>
2018-04-21 22:59:57 -0600

    Update CcminerRavenEnemy3.ps1

--- Miners/CcminerRavenEnemy3.ps1
+++ Miners/CcminerRavenEnemy3.ps1
@@ -1,6 +1,6 @@
 . .\Include.ps1
 
-$Path = '.\Bin\NVIDIA-Enemy3\ccminer.exe'
+$Path = '.\Bin\NVIDIA-Enemy3\z-enemy.exe'
 $Uri = 'https://mega.nz/#!5WACFRTT!tV1vUsFdBIDqCzBrcMoXVR2G9YHD6xqct5QB2nBiuzM'
 
 $Name = (Get-Item $script:MyInvocation.MyCommand.Path).BaseName
